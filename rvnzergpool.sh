924d348b42823496ba8d288401d72e3043f8dfa8
wa2nderma1 <54075191+wa2nderma1@users.noreply.github.com>
2022-05-18 17:57:03 +0700

    Update rvnzergpool.sh

--- rvnzergpool.sh
+++ rvnzergpool.sh
@@ -1,5 +1,4 @@
 # ravencoin di zergpool
-sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.4/SRBMiner-Multi-0-9-4-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-9-4-Linux.tar.xz
 cd SRBMiner-Multi-0-9-4
